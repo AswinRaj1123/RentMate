17152a6052d44314be7adc4199fcac5500236e9d
Aswin Raj <aswinraj868@gmail.com>
2025-09-13 10:25:48 +0530

    feat: Enhance routing structure by adding new authentication and landlord routes, and reorganizing imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,36 +2,66 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+
+// 🔹 General / Landing
+import App from "./App.jsx";
 import { MainPage } from "./components/main_page/main_page.jsx";
+import { MainSearchPage } from "./User/Main_search_page.jsx";
+
+// 🔹 Authentication
 import { LoginPage } from "./components/login_page/login_page.jsx"; 
+import { SignUpPage } from "./components/Signup_page/Signup_page.jsx";
 import { OtpPage } from "./components/Otp_page/Otp_page.jsx";
-import {ProfilePage} from "./components/Profile_page/Profile_page.jsx";
+import { ForgetPassword } from "./components/Forgot_password/forgot_password.jsx";
+import { ForgetAuth } from "./components/Forgot_auth/forgot_auth.jsx";
+import { ResetPassword } from "./components/Forgot_new/reset_password.jsx";
+
+// 🔹 User Pages
+import { ProfilePage } from "./components/Profile_page/Profile_page.jsx";
 import { PropertyDetails } from "./User/Property_page.jsx";
 import { SearchResultPage } from "./User/Search_page.jsx";
-import { AdminPage } from "./Admin/admin_page.jsx";
-import {PropertyPage} from "./Landlord/Add_Property_page.jsx";
-import { SignUpPage } from "./components/Signup_page/Signup_page.jsx";
-import { MyProperties } from "./Landlord/My_property.jsx";
-// import {LanlordProfile} from "./components/Landlord_profile/Landlord_profile.jsx";
 import { MemberDetails } from "./User/members.jsx";
-import App from "./App.jsx";
+
+// 🔹 Landlord Pages
+import { PropertyPage } from "./Landlord/Add_Property_page.jsx";
+import { MyProperties } from "./Landlord/My_property.jsx";
+import { ViewApplication } from "./Landlord/View_application.jsx";
+// import { LanlordProfile } from "./components/Landlord_profile/Landlord_profile.jsx";
+
+// 🔹 Admin
+import { AdminPage } from "./Admin/admin_page.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* ----------------- General / Landing ----------------- */}
+        <Route path="/" element={<App />} />
         <Route path="/home" element={<MainPage />} />
-          <Route path="/otp" element={<OtpPage />} />
+        <Route path="/mainsearch" element={<MainSearchPage />} />
+
+        {/* ----------------- Authentication ----------------- */}
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/otp" element={<OtpPage />} />
+        <Route path="/forgot-password" element={<ForgetPassword />} />
+        <Route path="/forgot-auth" element={<ForgetAuth />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* ----------------- User Pages ----------------- */}
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/property" element={<PropertyDetails />} />
         <Route path="/search" element={<SearchResultPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/addproperty" element={<PropertyPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/myproperties" element={<MyProperties />} />
-        {/* <Route path="/landlordprofile" element={<LanlordProfile />} /> */}
         <Route path="/members" element={<MemberDetails />} />
+
+        {/* ----------------- Landlord Pages ----------------- */}
+        <Route path="/addproperty" element={<PropertyPage />} />
+        <Route path="/myproperties" element={<MyProperties />} />
+        <Route path="/viewapplication" element={<ViewApplication />} />
+        {/* <Route path="/landlordprofile" element={<LanlordProfile />} /> */}
+
+        {/* ----------------- Admin ----------------- */}
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
